4f21830bcabb79aea631daa045269e58ab993104
howarudo <howardtangkulung@gmail.com>
2024-10-15 15:13:49 +0900

    done speech

---
+++ speech/scripts/run_convert_differential_f0.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#SBATCH -p v
+#SBATCH --gres=gpu:1
+export PATH=/home/app/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/home/app/cuda/lib64:$LD_LIBRARY_PATH
+poetry run python3 src/convert_differential_f0.py
